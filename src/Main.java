23a3371a87437fb922091119e922a730d7359019
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-06-30 14:58:31 +0200

    feat: crate new object student

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
 
+        Studente student1 = new Studente("Maria", "Rosa", 17);
+
+        System.out.println(student1.getStudentData());
+    }
 }
