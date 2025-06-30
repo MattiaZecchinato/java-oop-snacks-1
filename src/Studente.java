b1aabfd666f64ce10714d6fd564b6a680d861929
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-06-30 14:57:41 +0200

    feat: add attributes, constructor and methods for class Studente

--- src/Studente.java
+++ src/Studente.java
@@ -1,3 +1,48 @@
 public class Studente {
 
+    private static int ageMin = 15;
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Studente(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public void setFisrtName(String firstName) {
+        if (firstName != null) {
+            this.firstName = firstName;
+        }
+    }
+
+    public String getFisrtName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName != null) {
+            this.lastName = lastName;
+        }
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setAge(int age) {
+        if (age >= ageMin) {
+            this.age = age;
+        }
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getStudentData() {
+        return this.firstName + " " + this.lastName + ", " + this.age + " anni";
+    }
 }
