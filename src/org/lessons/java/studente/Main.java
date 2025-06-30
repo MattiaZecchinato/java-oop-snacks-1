57fd1367517147e50ed2f1fc8aa635b3c3cba2ae
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-06-30 17:47:56 +0200

    feat: add input for student data

--- src/org/lessons/java/studente/Main.java
+++ src/org/lessons/java/studente/Main.java
@@ -1,9 +1,25 @@
 package org.lessons.java.studente;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Studente student1 = new Studente("Maria", "Rosa", 17);
+        Scanner sc = new Scanner(System.in);
+
+        sc.nextLine();
+        System.out.println("Insert student's first name:");
+        String studentFirstName = sc.nextLine();
+
+        System.out.println("Insert student's last name:");
+        String studentLastName = sc.nextLine();
+
+        System.out.println("Insert student's age:");
+        int studentAge = sc.nextInt();
+
+        Studente student1 = new Studente(studentFirstName, studentLastName, studentAge);
 
         System.out.println(student1.getStudentData());
+
+        sc.close();
     }
 }
